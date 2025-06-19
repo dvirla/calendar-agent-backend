60d51aaa5d6c70e5f67a51ed7944ca6a9b134937
dvirla <dvirla84@gmail.com>
2025-06-19 08:42:38 +0300

    Renamed google id variable to service google id to resolve conflict with oauth.

--- app/waitinglist_service.py
+++ app/waitinglist_service.py
@@ -53,7 +53,7 @@ class WaitlistManager:
                 "private_key_id": os.getenv('GOOGLE_PRIVATE_KEY_ID'),
                 "private_key": os.getenv('GOOGLE_PRIVATE_KEY').replace('\\n', '\n'),
                 "client_email": os.getenv('GOOGLE_CLIENT_EMAIL'),
-                "client_id": os.getenv('GOOGLE_CLIENT_ID'),
+                "client_id": os.getenv('SERVICE_GOOGLE_CLIENT_ID'),
                 "auth_uri": "https://accounts.google.com/o/oauth2/auth",
                 "token_uri": "https://oauth2.googleapis.com/token",
                 "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
